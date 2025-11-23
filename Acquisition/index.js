172adf51190768106e2a4614518c6eac70d11531
ismail <alagele.ismail@gmail.com>
2025-11-23 14:12:05 +0100

    add port

--- Acquisition/index.js
+++ Acquisition/index.js
@@ -1,2 +1,10 @@
 import express from 'express';
 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listining on port ${PORT}`);
+});
+
