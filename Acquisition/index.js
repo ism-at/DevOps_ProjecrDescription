e4ed2c6859faa5b07b08f13fe0dbc07e7fcbabf5
ismail <alagele.ismail@gmail.com>
2025-11-23 14:50:26 +0100

    Implement MVC

--- Acquisition/index.js
+++ Acquisition/index.js
@@ -1,10 +1,11 @@
-import express from 'express';
+// import express from 'express';
 
-const app = express();
+// const app = express();
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`App listining on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`App listining on port ${PORT}`);
+// });
 
+// No more needed
